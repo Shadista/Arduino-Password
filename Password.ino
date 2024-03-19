0064a11fb189f5155730040c52b98b1c20530d48
Shadista <164022304+Shadista@users.noreply.github.com>
2024-03-19 22:41:28 +0800

    Update Password.ino

--- Password.ino
+++ Password.ino
@@ -53,6 +53,10 @@ void loop() {
       lcd.print("Enter Password:");
       checkPassword();
       buzzerCheck();
+    } else if (key == '*') {
+      changePassMode = true;
+      lcd.clear();
+      changePassword();
     }
   }
 }
@@ -64,7 +68,11 @@ void checkPassword() {
   char key;
 
   while ((key = keypad.getKey()) != '#'){
-    if (key != NO_KEY) {
+    if (key != NO_KEY) {if (key == 'A') {
+        backspace(inputPassword);
+        keyPress();
+      } else
+    {
       keyPress();
       inputPassword += key;
       lcd.setCursor(inputPassword.length() - 1, 1);
@@ -78,18 +86,19 @@ void checkPassword() {
       return;
     }
   }
+  }
 
   if (inputPassword == password) {
     lcd.clear();
-    lcd.print("Allowed");
+    lcd.print("Twinkle Twinkle!");
     correct = true;
     attempts = 0;
     ledCheck();
       if (correct) {
         digitalWrite(led[3], HIGH);
       }
-    tone(buzzer, 1000, 300);
-      delay(300);
+    twinkle(); //Credits to Abhishek Ghosh, Neerajkry, and Rohan Goyal for the song source code
+    //https://github.com/AbhishekGhosh/Arduino-Buzzer-Tone-Codes/tree/master
   } else {
     lcd.clear();
     lcd.print("Denied");
@@ -109,12 +118,17 @@ void changePassword() {
   char key;
 
   while ((key = keypad.getKey()) != '#'){
-    if (key != NO_KEY) {
+    if (key != NO_KEY) {if (key == 'A') {
+        backspace(oldPassword);
+        keyPress();
+      } else
+    {
       keyPress();
       oldPassword += key;
       lcd.setCursor(oldPassword.length() - 1, 1);
       lcd.print(key);
     }
+  }
   }
     if (oldPassword == password) {
     lcd.clear();
@@ -122,13 +136,18 @@ void changePassword() {
   char key;
   lcd.print("New Password:");
   while ((key = keypad.getKey()) != '#'){
-    if (key != NO_KEY) {
+    if (key != NO_KEY) {if (key == 'A') {
+        backspace(newPassword);
+        keyPress();
+      } else
+    {
       keyPress();
       newPassword += key;
       lcd.setCursor(newPassword.length() - 1, 1);
       lcd.print(key);
     }
   }
+}
   password = newPassword;
   lcd.clear();
   lcd.print("Password Changed!");
@@ -149,15 +168,21 @@ void changePassword() {
 }
 
 
+
 void setPassword() {
   char key;
   while ((key = keypad.getKey()) != '#'){
-    if (key != NO_KEY) {
+    if (key != NO_KEY) {if (key == 'A') {
+        backspace(newPassword);
+        keyPress();
+      } else
+    {
       keyPress();
       newPassword += key;
       lcd.setCursor(newPassword.length() - 1, 1);
       lcd.print(key);
     }
+  }
   }
   password = newPassword;
   newPassword = "";
@@ -165,6 +190,7 @@ void setPassword() {
   lcd.clear();
   lcd.print("Password Done!");
 }
+
 
 void ledCheck() {
   if (attempts == 0) {
@@ -204,4 +230,54 @@ void buzzerCheck() {
 
 void keyPress() {
   tone(buzzer, 1000, 50);
+}
+void backspace(String &str) {
+  if (str.length() > 0) {
+    str.remove(str.length() - 1);
+    lcd.setCursor(str.length(), 1);
+    lcd.print(" ");
+  }
+}
+
+//Credits to Abhishek Ghosh, Neerajkry, and Rohan Goyal for the song source code
+
+int length = 15; // the number of notes
+
+//twinkle twinkle little star
+char notes[] = "ccggaag ffeeddc ggffeed ggffeed ccggaag ffeeddc "; // a space represents a rest
+int beats[] = { 1, 1, 1, 1, 1, 1, 2, 1, 1, 1, 1, 1, 1, 2, 4 };
+int tempo = 300;
+
+void playTone(int tone, int duration) {
+  for (long i = 0; i < duration * 1000L; i += tone * 2) {
+    digitalWrite(buzzer, HIGH);
+    delayMicroseconds(tone);
+    digitalWrite(buzzer, LOW);
+    delayMicroseconds(tone);
+  }
+}
+
+void playNote(char note, int duration) {
+  char names[] = { 'c', 'd', 'e', 'f', 'g', 'a', 'b', 'C' };
+  int tones[] = { 1915, 1700, 1519, 1432, 1275, 1136, 1014, 956 };
+  
+  // play the tone corresponding to the note name
+  for (int i = 0; i < 8; i++) {
+    if (names[i] == note) {
+      playTone(tones[i], duration);
+    }
+  }
+}
+
+void twinkle() {
+  for (int i = 0; i < length; i++) {
+    if (notes[i] == ' ') {
+      delay(beats[i] * tempo); // rest
+    } else {
+      playNote(notes[i], beats[i] * tempo);
+    }
+    
+    // pause between notes
+    delay(tempo / 2); 
+  }
 }
